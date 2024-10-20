805dff9beabef85efe1d1a0afa93e705896c136e
Thundem <vovkandrij7@gmail.com>
2024-10-21 00:11:57 +0300

    init project

---
+++ server/ContentFlow/src/main/java/com/coursework/ContentFlow/ContentFlowApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.ContentFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentFlowApplication.class, args);
+	}
+
+}
---
+++ server/ContentFlow/src/test/java/com/coursework/ContentFlow/ContentFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.ContentFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
