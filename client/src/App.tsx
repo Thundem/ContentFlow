e153eb26098576e781c1d4eaed1fe3d1b6a06eb9
Thundem <vovkandrij7@gmail.com>
2024-10-26 01:49:40 +0300

    added Route for navigate on site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import PostList from './components/PostList';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+// import About from './components/About'; // Приклад іншої сторінки
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <Header />
-            <PostList />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<PostList />} /> 
+                    <Route path="/login" element={<Login />} /> 
+                    <Route path="/signup" element={<SignUp />} /> 
+                    {/* <Route path="/about" element={<About />} /> */}
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
