50a06b9f1add7e195af2026bdd0214893698c458
Thundem <vovkandrij7@gmail.com>
2024-10-26 18:45:14 +0300

    updated types

--- client/src/components/types.tsx
+++ client/src/components/types.tsx
@@ -10,3 +10,27 @@ export interface SignUpData {
     confirmPassword: string;
     IsAccepted: boolean;
 }
+
+export interface Comment {
+    id: number;
+    text: string;
+}
+
+export interface PostData {
+    id: number;
+    title: string;
+    content: string;
+    likes: number;
+    user: { id: number; username: string } | null;
+    comments: Comment[];
+    userId: number | null;
+}
+
+export interface PostProps {
+    id: number;
+    title: string;
+    content: string;
+    likes: number;
+    comments: Comment[];
+    userId: number | null; 
+}
