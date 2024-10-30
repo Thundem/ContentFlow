d063433cbdfb530607fa3d3c350ef1fb9f65994f
Thundem <vovkandrij7@gmail.com>
2024-10-30 13:01:12 +0200

    added JwtRequestFilter for JWT token validation and authentication

---
+++ server/ContentFlow/src/main/java/com/coursework/ContentFlow/configurations/JwtRequestFilter.java
@@ -0,0 +1,84 @@
+package com.coursework.ContentFlow.configurations;
+
+import com.coursework.ContentFlow.services.UserService;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    @Lazy
+    private UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        // Виключаємо URL-адреси автентифікації
+        String path = request.getRequestURI();
+        if (path.startsWith("/api/auth")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        final String requestTokenHeader = request.getHeader("Authorization");
+
+        if (requestTokenHeader != null) {
+            System.out.println("Authorization header: " + requestTokenHeader);
+        }
+
+        String email = null;
+        String jwtToken = null;
+
+        // Логування заголовків для діагностики
+        Enumeration<String> headerNames = request.getHeaderNames();
+        System.out.println("Headers:");
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            System.out.println(headerName + ": " + request.getHeader(headerName));
+        }
+
+        // JWT токен має формат "Bearer token"
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+            try {
+                email = jwtUtil.extractUsername(jwtToken);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Неможливо отримати JWT Token");
+            } catch (ExpiredJwtException e) {
+                System.out.println("JWT Token прострочений");
+            }
+        } else {
+            logger.warn("JWT Token не починається з 'Bearer '");
+        }
+
+        // Валідація токена
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtUtil.validateToken(jwtToken, email)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+                        email, null, null);
+                usernamePasswordAuthenticationToken
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
