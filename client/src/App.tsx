c31d58c144b74f7e8a9cb796092f1960bf767736
Thundem <vovkandrij7@gmail.com>
2024-11-09 17:09:13 +0200

    added page 404 and create post page, and moved Route to main.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import PostList from './components/PostList';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import CreatePost from './components/CreatePost';
+import PageNotFound from './components/PageNotFound';
 import PrivateRoute from './components/PrivateRoute';
 
 const App: React.FC = () => {
     return (
-            <Router>
-                <div className="App">
-                    <Header />
-                    <Routes>
-                        <Route path="/" element={<PrivateRoute element={<PostList />} />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<SignUp />} />
-                    </Routes>
-                </div>
-            </Router>
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route path="/" element={<PrivateRoute element={<PostList />} />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<SignUp />} />
+                <Route path="/create" element={<CreatePost />} />
+                <Route path="*" element={<PageNotFound />} />
+            </Routes>
+        </div>
     );
 };
 
