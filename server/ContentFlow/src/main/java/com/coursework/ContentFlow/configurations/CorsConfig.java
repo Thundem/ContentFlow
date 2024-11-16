5755bf123e1dc9f4cd914566a7c2c928ccd5bf99
Thundem <vovkandrij7@gmail.com>
2024-11-16 23:02:44 +0200

    added patch method to cors

--- server/ContentFlow/src/main/java/com/coursework/ContentFlow/configurations/CorsConfig.java
+++ server/ContentFlow/src/main/java/com/coursework/ContentFlow/configurations/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173") // Ваш порт фронтенду
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
