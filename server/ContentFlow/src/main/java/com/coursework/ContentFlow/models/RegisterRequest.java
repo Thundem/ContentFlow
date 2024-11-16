1fd8fdd9335779387852bf5574ebc20c53d0684d
Thundem <vovkandrij7@gmail.com>
2024-11-16 02:57:15 +0200

    added lombok annotation

--- server/ContentFlow/src/main/java/com/coursework/ContentFlow/models/RegisterRequest.java
+++ server/ContentFlow/src/main/java/com/coursework/ContentFlow/models/RegisterRequest.java
@@ -1,14 +1,18 @@
 package com.coursework.ContentFlow.models;
 
 import com.coursework.ContentFlow.models.enums.Gender;
+import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 
+@Data
 public class RegisterRequest {
     private String username;
     private String email;
+    private String name;
+    private String surname;
     private String password;
     private Gender gender;
 
@@ -16,23 +20,4 @@ public class RegisterRequest {
     private LocalDate dateOfBirth;
 
     private MultipartFile avatar;
-
-    // Геттери та сеттери
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
-    public Gender getGender() { return gender; }
-    public void setGender(Gender gender) { this.gender = gender; }
-
-    public LocalDate getDateOfBirth() { return dateOfBirth; }
-    public void setDateOfBirth(LocalDate dateOfBirth) { this.dateOfBirth = dateOfBirth; }
-
-    public MultipartFile getAvatar() { return avatar; }
-    public void setAvatar(MultipartFile avatar) { this.avatar = avatar; }
 }
