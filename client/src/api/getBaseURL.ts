7c160ccf47eff937faa8ef0a1dc4c5d6f9cc494f
Thundem <vovkandrij7@gmail.com>
2024-11-17 23:06:18 +0200

    updated path to backend

--- client/src/api/getBaseURL.ts
+++ client/src/api/getBaseURL.ts
@@ -1,6 +1,6 @@
 const getBaseURL = () => {
     if (process.env.NODE_ENV === 'production') {
-      return 'https://contentflow.onrender.com';
+      return 'https://contentflow.onrender.com:8080';
     }
     return 'http://localhost:8080';
 };
